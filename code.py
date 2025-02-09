f2404e22be822757a9df8583d1f4d55155aedc36
fsck <inaforloop@gmail.com>
2025-02-08 20:50:25 -0500

    added a file to we can send multiple commands to the keyboard

--- code.py
+++ code.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import time
+
 import usb_hid
 import wifi
 import socketpool
@@ -10,7 +12,9 @@ import digitalio
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_httpserver import Server, Request, Response, Route
+
 
 from adafruit_httpserver.status import *
 
@@ -18,9 +22,6 @@ LISTENING_PORT = 8080
 
 
 def wifi_connect():
-
-    hello = os.getenv("HELLO")
-    print(hello)
 
     ssid = os.getenv("WIFI_SSID")
     password = os.getenv("WIFI_PASSWORD")
@@ -30,40 +31,76 @@ def wifi_connect():
     print(f"Connected to {ssid}")
 
 
+def build_keycode_strings() -> dict[str, int]:
+
+    keys: dict[str, int] = {}
+    code = Keycode()
+
+    for attr in dir(code):
+        if attr.startswith("_"):
+            continue
+        if False == attr[0].isupper():
+            continue
+
+        keys[attr] = int(getattr(code, attr))
+
+    return keys
+
+
 class KeyStroke:
 
     def __init__(self) -> None:
         self.kbd = Keyboard(usb_hid.devices)
+        self.layout = KeyboardLayoutUS(self.kbd)
+        self.keycode_table = build_keycode_strings()
+
+    def __command_handler(self, cmd: dict[str, str]) -> None:
+        if "sleep" in cmd:
+            time.sleep(int(cmd["sleep"]))
+        elif "write" in cmd:
+            self.layout.write(cmd["write"])
+        elif "keycodes" in cmd:
+            codes = cmd["keycodes"].split(",")
+            if 1 == len(codes):
+                assert codes[0] in self.keycode_table
+                self.kbd.send(self.keycode_table[codes[0]])
+            elif 2 == len(codes):
+                assert codes[0] in self.keycode_table
+                assert codes[1] in self.keycode_table
+                self.kbd.press(self.keycode_table[codes[0]])
+                self.kbd.send(self.keycode_table[codes[1]])
+                self.kbd.release()
+            else:
+                count = len(codes)
+                raise ValueError(f"Too many keycodes ({count})")
 
     def api_wakeup(self, request: Request) -> Response:
         print("sending ESC")
         self.kbd.send(Keycode.ESCAPE)
         return Response(request, status=OK_200)
 
-    def api_hello(self, request: Request) -> Response:
-        # Hello,
-        self.kbd.press(Keycode.SHIFT)
-        self.kbd.send(Keycode.H)
-        self.kbd.release(Keycode.SHIFT)
-        self.kbd.send(Keycode.E)
-        self.kbd.send(Keycode.L)
-        self.kbd.send(Keycode.L)
-        self.kbd.send(Keycode.O)
-        self.kbd.send(Keycode.COMMA)
-        # World
-        self.kbd.press(Keycode.SHIFT)
-        self.kbd.send(Keycode.W)
-        self.kbd.release(Keycode.SHIFT)
-        self.kbd.send(Keycode.O)
-        self.kbd.send(Keycode.R)
-        self.kbd.send(Keycode.L)
-        self.kbd.send(Keycode.D)
-        self.kbd.press(Keycode.SHIFT)
-        self.kbd.send(Keycode.ONE)
-        self.kbd.release(Keycode.SHIFT)
-        self.kbd.send(Keycode.ENTER)
+    def api_type(self, request: Request) -> Response:
 
-        return Response(request, status=OK_200)
+        data: dict[str, list] | None = request.json()  # type: ignore
+
+        if data is None or "commands" not in data:
+            return Response(request, status=BAD_REQUEST_400)
+
+        commands: list[dict[str, str]] = data["commands"]
+
+        try:
+            for c in commands:
+                self.__command_handler(c)
+
+            return Response(request, status=OK_200)
+        except AssertionError as e:
+            err_msg = f"assertion: {e}"
+        except ValueError as e:
+            err_msg = f"value error: {e}"
+        except Exception as e:
+            err_msg = f"giant exception: {e}"
+
+        return Response(request, body=err_msg, status=OK_200)
 
 
 def main() -> int:
@@ -85,7 +122,7 @@ def main() -> int:
 
         routes = [
             Route("/api/wakeup", "GET", ks.api_wakeup),
-            Route("/api/hello", "GET", ks.api_hello)
+            Route("/api/type", "POST", ks.api_type)
         ]
 
         server.add_routes(routes)
